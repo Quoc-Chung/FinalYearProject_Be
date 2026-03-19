420630cdd23b4d7412233af4283c5af3ee3bc32c
quocchungdev <Chungtq@flexinet.com.vn>
2026-03-19 07:38:33 +0700

    base project

---
+++ src/main/java/com/quocchung/cntt1/techcycle_system/TechcycleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.quocchung.cntt1.techcycle_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechcycleSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechcycleSystemApplication.class, args);
+	}
+
+}
